4315c6403a06d7d97381dfad58e22c33499a99e1
Николай <nikolay@MacBook-Pro-Nikolaj.local>
2026-06-03 23:14:58 +0300

    step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+import genDiff from '../src/index.js'
+
 import { program } from 'commander'
 
 program
@@ -7,5 +9,11 @@ program
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1')
     .option('-f, --format [type]', 'output format')
+    .argument('<file1>')
+    .argument('<file2>')
+    .action((filepath1, filepath2, options) => {
+        genDiff(filepath1, filepath2)
+    }) 
 
 program.parse()
+
--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-const genDiff = (filepath1, filepath2) => {
-    return 'gendiff'
+import fs from 'node:fs'
+import path from 'node:path'
+
+const genDiff = (filepath1, filepath2, format) => {
+    const path1 = path.isAbsolute(filepath1) ? filepath1 : path.resolve(filepath1)
+    const path2 = path.isAbsolute(filepath2) ? filepath2 : path.resolve(filepath2)
+    const data1 = fs.readFileSync(path1, 'utf-8')
+    const data2 = fs.readFileSync(path2, 'utf-8')
+    const ext = path.extname(filepath1)
+    let parse1
+    let parse2
+    if (ext === '.json') {
+        parse1 = JSON.parse(data1)
+        parse2 = JSON.parse(data2)
+    }
+
+    return ''
 }
 
 export default genDiff
